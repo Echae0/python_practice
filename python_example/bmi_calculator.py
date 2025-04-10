1d8ff0a42a032c8924954e2b6978dbbc686e1b2d
Echae0 <edujcm01@gmail.com>
2025-04-11 01:28:04 +0900

    bmi finish

--- python_example/bmi_calculator.py
+++ python_example/bmi_calculator.py
@@ -1,6 +1,6 @@
 weight = float(input("체중을 입력하세요(kg): "))
 height = float(input("키를 입력하세요 (cm): "))
-BMI = weight / (height ** 2)
-print("체중(kg): ", weight, \n
-      "키(cm): ", height, \n
-      "BMI: ", BMI)
+BMI = weight / ((height/100) ** 2)
+print("체중(kg): ", round(weight,0))
+print("키(cm): ", round(height,0))
+print("BMI: ", round(BMI, 1))
