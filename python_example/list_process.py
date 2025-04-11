cb60e0af47e8685046cfdb5775e0935701310c87
Echae0 <edujcm01@gmail.com>
2025-04-11 15:10:11 +0900

    finish list process

---
+++ python_example/list_process.py
@@ -0,0 +1,17 @@
+#["apple", "banana", "cherry"] 리스트에 "orange"를 추가하세요.
+fruit = ["apple", "banana", "cherry"]
+fruit.append("orange")
+print(fruit)
+
+#[10, 20, 30] 리스트의 모든 요소의 합을 구하세요.
+list1 = [10,20,30]
+print(sum(list1))
+
+#[1, 2, 3, 4, 5] 리스트의 요소들을 역순으로 출력하세요.
+list2 = [1,2,3,4,5]
+print(list2[::-1])
+
+#[5, 2, 8, 1, 9] 리스트를 오름차순으로 정렬하세요.
+list3 = [5,2,8,1,9]
+list3.sort()
+print(list3)
